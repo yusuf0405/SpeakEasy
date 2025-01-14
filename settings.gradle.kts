22e9a2a8080cc38330a2ad80613000ffdf5e904c
Joseph <planetapluton888@gmail.com>
2025-01-14 15:00:35 +0600

    Add logic for automatically registers project modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,25 +30,49 @@ dependencyResolutionManagement {
     }
 }
 
-include(
-    ":composeApp",
-    ":core",
-    ":ui-core",
-    ":data",
-    ":domain",
-    ":ui-components",
-    ":feature-modules:translator:translator-api",
-    ":feature-modules:translator:translator-impl",
-    ":feature-modules:history:history-api",
-    ":feature-modules:history:history-impl",
-    ":feature-modules:speech:speech-api",
-    ":feature-modules:speech:speech-impl",
-    ":feature-modules:settings:settings-api",
-    ":feature-modules:settings:settings-impl",
-    ":feature-modules:languages:languages-api",
-    ":feature-modules:languages:languages-impl",
-    ":feature-modules:camera-capture:camera-capture-api",
-    ":feature-modules:camera-capture:camera-capture-impl",
-    ":feature-modules:permission:permission-api",
-    ":feature-modules:permission:permission-impl",
-)
+includeSubprojects()
+
+fun includeSubprojects() {
+    val moduleBlackList = listOf("infrastructure")
+    initModules(rootDir, depth = 1, moduleBlackList)
+    includeModulesIn("apps", depth = 1)
+    includeModulesIn("feature-modules", depth = 2)
+}
+
+fun includeModulesIn(directoryName: String, depth: Int) {
+    initModules(File(rootDir, directoryName), depth)
+}
+
+/**
+ * Automatically registers project modules
+ *
+ * @param sourceDir root directory for module search
+ * @param depth maximum recursion depth
+ * @* @param module BlackList list of excluded modules
+ */
+fun initModules(sourceDir: File, depth: Int, moduleBlackList: List<String> = emptyList()) {
+    sourceDir.walk()
+        .maxDepth(depth)
+        .filter { it.shouldIncludeModule(sourceDir, moduleBlackList) }
+        .forEach { moduleDir ->
+            val moduleName = moduleDir.toModuleName()
+            include(moduleName)
+            project(moduleName).projectDir = moduleDir
+        }
+}
+
+fun File.shouldIncludeModule(sourceDir: File, moduleBlackList: List<String>): Boolean {
+    return name !in moduleBlackList &&
+            isDirectory &&
+            this != sourceDir &&
+            isGradleProject()
+}
+
+fun File.toModuleName(): String {
+    return path.substringAfter(rootDir.name).replace(File.separatorChar, ':')
+}
+
+fun File.isGradleProject(): Boolean {
+    return isDirectory &&
+            listOf("build.gradle.kts", "build.gradle").any { File(this, it).exists() }
+}
